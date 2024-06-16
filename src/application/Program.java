6c11feba4e7ef5a09841f3e693efd603aad5327a
Rômulo Ribeiro <romuloraff@gmail.com>
2024-06-16 17:03:42 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,43 +13,35 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Número do quarto: ");
 		int number = sc.nextInt();
 		System.out.print("Check-in data (dd/MM/YYYY): ");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.print("Check-out data (dd/MM/YYYY): ");
 		Date checkOut = sdf.parse(sc.next());
-		
+
 		if (!checkOut.after(checkIn)) {
 			System.out.println("A data de check-out deve ser posterior ao check-in: ");
-			
-		}
-		else {
+
+		} else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reserva: " + reservation);
-			
+
 			System.out.println();
 			System.out.println("Insira os dados para atualizar a reserva: ");
 			System.out.print("Check-in data (dd/MM/YYYY): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out data (dd/MM/YYYY): ");
 			checkOut = sdf.parse(sc.next());
-			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Erro na reserva: As datas de reserva para atualização devem ser datas futuras");
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Erro na reserva: " + error);
+			} else {
+            	System.out.println("Reserva: " + reservation);
 			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Erro na reserva: A data de check-out deve ser posterior ao check-in: ");
-			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reserva: " + reservation);
-			}
-       	
 		}
 		sc.close();
 	}
-
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,13 +5,13 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
-	
+
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
@@ -29,7 +29,7 @@ public class Reservation {
 	public Date getCheckin() {
 		return checkIn;
 	}
-	
+
 	public Date getCheckout() {
 		return checkOut;
 	}
@@ -38,23 +38,25 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDates(Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "As datas de reserva para atualizações devem ser datas futuras";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "A data de check-out deve ser posterior ao check-in";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room "
-			+ roomNumber
-			+ ", check-in: "
-			+ sdf.format(checkIn)
-			+ ", check-out: "
-			+ sdf.format(checkOut)
-			+ ", "
-			+ duration()
-			+ " noites";
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " noites";
 	}
-	
+
 }
